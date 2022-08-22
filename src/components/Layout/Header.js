87b6241499e86ecb6fe3220bceffd92a5fdf72c9
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-22 15:36:04 -0500

    initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./header.modules.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img src=""></img>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
