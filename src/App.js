666880b1702b61a83e9ddb56d8f3ab2a4cff26fc
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-23 13:32:30 -0500

    modal added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Header from "./components/Layout/Header";
 
 import Meals from "./components/Meals/Meals";
 
+import Cart from "./components/Cart/Cart";
+
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
