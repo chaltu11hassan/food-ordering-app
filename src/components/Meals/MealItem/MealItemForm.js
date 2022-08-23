2acf8afa06d6dd269ba1536bee268249f80d558a
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-08-23 10:29:30 -0500

    form updated

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,14 +1,26 @@
 import React from "react";
 
-import classes from './MealItemForm.module.css';
+import classes from "./MealItemForm.module.css";
 
-const MealItemForm =()=>{
-    return(
-        <form className={classes.form}>
-            <input></input>
-            <button>+ Add</button>
-        </form>
-    )
-}
+import Input from "../../UI/Input";
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+ Add</button>
+    </form>
+  );
+};
 
 export default MealItemForm;
